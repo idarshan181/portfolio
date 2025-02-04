4eac4391815fed71c875d70c5322edb585d607e6
Darshan Patel <ddpatel97@gmail.com>
2025-02-04 00:22:02 -0330

    fix: fixed base template to have footer and different navbar style

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -1,42 +1,55 @@
-import { AppConfig } from '@/utils/AppConfig';
-import Link from 'next/link';
+import Footer from '@/components/Footer';
+import TimeDisplay from '@/components/TimeDisplay';
+import { display, person } from '@/resources';
 
-export const BaseTemplate = (props: {
+type BaseTemplateProps = {
   leftNav: React.ReactNode;
   rightNav?: React.ReactNode;
+  centerNav?: React.ReactNode;
   children: React.ReactNode;
-}) => {
+};
+
+export const BaseTemplate: React.FC<BaseTemplateProps> = (props) => {
   // const t = useTranslations('BaseTemplate');
 
   return (
-    <div className="w-full px-1 text-gray-700 antialiased">
+    <div className="w-full px-1 antialiased">
       <div className="mx-auto">
-        <header className="h-20 items-center px-2">
-          <div className="flex h-full items-center justify-around">
-            <nav>
-              <h1 className="h-full items-center text-3xl font-bold text-gray-900">
+        <header className="fixed top-2 z-50 mx-auto flex h-14 w-full items-center justify-around px-5">
+          <div className="">
+            {display.location && <span>{person.location}</span>}
+          </div>
+          <div className="mx-auto flex size-full max-w-fit items-center justify-around rounded-lg border-2 bg-background px-4 shadow-lg">
+            {/* <nav>
+              <h1 className="h-full items-center text-3xl font-bold text-foreground">
                 <Link href="/">
                   {AppConfig.name}
                 </Link>
               </h1>
-            </nav>
-
+            </nav> */}
             <nav>
               <ul className="flex h-full flex-wrap items-center gap-x-5 text-lg font-medium">
                 {props.leftNav}
               </ul>
             </nav>
-
+            <nav>
+              <ul className="flex h-full flex-wrap items-center gap-x-2 text-lg font-medium">
+                {props.centerNav}
+              </ul>
+            </nav>
             <nav>
               <ul className="flex h-full flex-wrap items-center gap-x-2 text-lg font-medium">
                 {props.rightNav}
               </ul>
             </nav>
           </div>
+          <div className="w-fit">
+            {display.time && <TimeDisplay timeZone={person.timezone} />}
+          </div>
         </header>
 
-        <main>{props.children}</main>
-
+        <main className="mx-40 pt-20">{props.children}</main>
+        <Footer />
       </div>
     </div>
   );
