9a707ab422ab42c44281ba23d59c03a909247d0b
Darshan Patel <ddpatel97@gmail.com>
2025-02-07 14:07:37 -0330

    fix: modified sitemap.xml

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,10 +3,26 @@ import { getBaseUrl } from '@/utils/Helpers';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        crawlDelay: 1, // Optional: Set a crawl delay for Googlebot
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        crawlDelay: 1, // Optional: Set a crawl delay for Bingbot
+      },
+      {
+        userAgent: 'BadBot', // Example of blocking a specific bot
+        disallow: '/',
+      },
+    ],
     sitemap: `${getBaseUrl()}/sitemap.xml`,
   };
 }
