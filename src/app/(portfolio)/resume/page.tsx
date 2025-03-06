2f2aaf22e3c8bc7c08cf6f8636c4c6c37eae793c
Darshan Patel <ddpatel97@gmail.com>
2025-03-06 15:18:46 -0330

    fix: pdfjs worker

--- src/app/(portfolio)/resume/page.tsx
+++ src/app/(portfolio)/resume/page.tsx
@@ -2,7 +2,13 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { cn } from '@/lib/utils';
 import { useResizeObserver } from '@wojtekmaj/react-hooks';
 import { Download } from 'lucide-react';
@@ -11,10 +17,9 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.8.69/pdf.worker.mjs',
-  import.meta.url,
-).toString();
+if (typeof window !== 'undefined') {
+  pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.8.69/pdf.worker.mjs';
+}
 
 const maxWidth = 800;
 
@@ -44,12 +49,24 @@ export default function ResumePage() {
 
   return (
     <div className="container mx-auto py-8 px-4">
-      <Card className={cn('mx-auto max-w-4xl shadow-lg overflow-hidden max-h-screen', containerWidth)}>
+      <Card
+        className={cn(
+          'mx-auto max-w-4xl shadow-lg overflow-hidden max-h-screen',
+          containerWidth,
+        )}
+      >
         <CardHeader className="flex flex-row justify-between items-center p-4">
           <CardTitle>Darshan's Resume</CardTitle>
           <CardDescription>
-            <Button className="cursor-pointer self-end place-self-end mr-0 ml-auto text-white" size="sm" asChild>
-              <a href="/resume/Darshan_Resume.pdf" download="DarshanPatel-resume.pdf">
+            <Button
+              className="cursor-pointer self-end place-self-end mr-0 ml-auto text-white"
+              size="sm"
+              asChild
+            >
+              <a
+                href="/resume/Darshan_Resume.pdf"
+                download="DarshanPatel-resume.pdf"
+              >
                 <Download className="mr-2 h-4 w-4" />
                 Download
               </a>
@@ -57,26 +74,35 @@ export default function ResumePage() {
           </CardDescription>
         </CardHeader>
         <CardContent className="p-6">
-          <div ref={setContainerRef} className="w-full h-[80vh] overflow-y-auto border rounded-lg shadow-md p-2">
+          <div
+            ref={setContainerRef}
+            className="w-full h-[80vh] overflow-y-auto border rounded-lg shadow-md p-2"
+          >
             <Document
               file="/resume/Darshan_Resume.pdf"
               onLoadSuccess={onDocumentLoadSuccess}
               onLoadError={onDocumentLoadError}
               className="dark:invert dark:brightness-90 dark:contrast-110 dark:hue-rotate-90 rounded-lg"
             >
-
-              {numPages && Array.from({ length: numPages }, (_el, index) => (
-                <div key={`page_${index + 1}`} className="rounded-lg overflow-hidden">
-                  <Page
-                    pageNumber={index + 1}
-                    renderTextLayer
-                    renderAnnotationLayer
-                    width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth}
-                    className=""
-                  />
-                </div>
-              ))}
-
+              {numPages
+                && Array.from({ length: numPages }, (_el, index) => (
+                  <div
+                    key={`page_${index + 1}`}
+                    className="rounded-lg overflow-hidden"
+                  >
+                    <Page
+                      pageNumber={index + 1}
+                      renderTextLayer
+                      renderAnnotationLayer
+                      width={
+                        containerWidth
+                          ? Math.min(containerWidth, maxWidth)
+                          : maxWidth
+                      }
+                      className=""
+                    />
+                  </div>
+                ))}
             </Document>
           </div>
         </CardContent>
